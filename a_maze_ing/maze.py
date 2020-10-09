9af597b324407d1d26f2ebea840f231b0f674bc2
Sam <samysaad01@gmail.com>
2020-10-09 18:18:25 +0200

    3rd commit

--- a_maze_ing/maze.py
+++ a_maze_ing/maze.py
@@ -4,28 +4,24 @@ class Maze():
     height = 15
     filename = 'a_maze_ing.txt'
 
-    def draw_font_lab(self):      
+    def generate_empty_lab(self):
         """
         draw_font method plots the font of the lab onto a text file:
         no walls, only empty paths.
-        
-        Returns:
-            list: list of all case arranged in list of list of 
-        """
-        axe_x = axe_y = []
-        with open(self.filename, 'w+') as f:
-            for i in range(self.width):
-                axe_x[i] = 0
-            for j in range(self.height):
-                axe_y[j] = axe_x
-            for item in axe_y:
-                f.write(f"{item}\n")
-        return axe_y
 
+        Returns:
+            list: list of all case arranged in list of list of
+        """
+        with open(self.filename, 'w+') as f:
+            f.write(" ")
+
+
+Maze.generate_empty_lab()
+"""
     def display_lab(self):
-        """
+        
         display_lab method prints lab layout, from text file to console.
-        """
+        
         with open('a_maze_ing.txt', 'r') as f:
             print(f.read())
 
@@ -53,10 +49,10 @@ class Positions():
     pos_y: int
 
     def reference_position():
-        """
+        
         reference_position method defines point of origin and assign position
         to each font item (empty path).
-        """
+        
         pass
 
     def up(self):
@@ -82,3 +78,4 @@ class SomeItems(Maze, Positions):
     items_proprety = ['mac', 'gard', 'lab', None]
     items_state = ['to_find', 'found', 'used', 'made']
     pass
+"""
--- a_maze_ing/py.py
+++ a_maze_ing/py.py
@@ -1,14 +1,49 @@
-axe_x = []
-axe_y = []
+def generate_lab():
+    with open('a_maze_ing.txt', 'w') as f:
+        for j in range(15):
+            for i in range(15):
+                f.write("1")
+            f.write('\n')
 
-for i in range(15):
-    axe_x.append(0)
-for j in range(15):
-    axe_y.append(axe_x)
 
-print(axe_x)
-print(axe_y)
+def lab_to_list():
+    with open('a_maze_ing.txt', 'r') as f:
+        list_of_lines = f.readlines()
+        list_of_lines = [line[:-1] for line in list_of_lines]
+    return list_of_lines
 
-with open('test.txt', 'w+') as f:
-    for item in axe_y:
-        f.write(f"{item}\n")
+
+def put_in_lab(master_list: list, x: int, y: int, object_to_place):
+    """
+    Put an element in the lab.
+
+    Args:
+        master_list (list): list representing the lab
+        x (int): abs postion of object
+        y (int): ord position of object
+        object_to_place (str): element to place in lab.
+
+    Returns:
+        master_list (list): list representing the lab with the new element in it
+    """
+    line = list(master_list[y])
+    line[x-1] = object_to_place
+    master_list[y] = line
+    return master_list
+
+def del_from_lab(master_list: list, x: int, y: int):
+    """
+    Del an element from the lab
+
+    Args:
+        master_list (list): list representing the lab
+        x (int): abs postion of object
+        y (int): ord position of object
+
+    Returns:
+        master_list (list): list representing the lab with desired element removed
+    """
+    line = list(master_list[y])
+    line[x-1] = ' '
+    master_list[y] = line
+    return master_list
