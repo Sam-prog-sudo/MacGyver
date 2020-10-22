877297563acb3f77ea01bc8c4c5be4bebc776b30
Sam <samysaad01@gmail.com>
2020-10-22 19:06:17 +0200

    Total redisign of project.
    Maze class:
    - init method
    - method to create a list for every part of lab
    - random position from list of empty path
    TODO: display lab in console
    
    Characters class:
    - init method
    - disabled method
    TODO: macgyver assemble items
    or should it be in item class
    
    SomeItem class:
    - init method
    - item is picked up
    TODO: items are assembled
    or should it be in Character class
    
    NB: - wall are stored as list of tuple wiches describe their positions
            - every other objet stored in a list of class instance,
            with position  attribut

--- a_maze_ing/maze.py
+++ a_maze_ing/maze.py
@@ -1,134 +1,99 @@
+import random
+
 
 class Maze():
-    width = 15
-    height = 15
+
+    # constants to be placed in some other file
+    width = 0
+    height = 0
     filename = 'a_maze_ing.txt'
     path = " "
     wall = "X"
     gard = "G"
     macgyver = "M"
     start = "S"
-    exit = "E"
+    finish = "F"
+    needle = 'n'
+    tube = 't'
+    ether = 'e'
 
-    def lab_txt_to_list(self):
-        """
-        Convert text file which represents the the labyrinth layout into a list.
+    def __init__(self):
+        self.list_walls = []
+        self.list_path = []
+        self.list_pers = []
+        self.list_items = []
+        self.start.position = ()
+        self.finish.position = ()
 
-        The text file, which represents the labyrinth, is stored as a list of rows.
-        The keys of this list represents the row number.
-        An element of this list represents all the elements in a row.
-
-        Returns:
-            list:
-                a list of labyrinth rows
-        """
-        master_list = []
+    def create_lists_of_everything(self):
         with open('a_maze_ing.txt', 'r') as m:
+            y = 0           # row number
             for line in m:
-                line = line.strip()
-                master_list.append(line)
-                print
-                for i in range(len(master_list)):
-                    master_list[i] = list(master_list[i])
-            return master_list
+                x = 0       # column number
+                for char in line:
+                    if char != '\n':
+                        if char == 'X':
+                            self.list_walls.append((y, x))
+                        elif char == ' ':
+                            self.list_path.append((y, x))
+                        elif char == 'S':
+                            self.start.position = (y, x)
+                        elif char == 'F':
+                            self.start.position = (y, x)
+                    x += 1  # row increment
+                y += 1      # column increment
+            self.width = x
+            self.height = y
 
-    def access_elt(master_list: list, x: int, y: int):
-        """
-        Access a certain element of the labyrinth and returns it.
+    def pick_random_empty_position(self):
+        return random.choice(Maze.list_path)
 
-        Args:
-            master_list (list):
-                list representing the lab
-            x (int):
-                column postion of object
-            y (int):
-                row position of object
-
-        Returns:
-            int:
-                an element of the lab
-        """
-        return master_list[y][x]
-
-    def put_in_lab(master_list: list, x: int, y: int, object_to_place):
-        """
-        Put an element in the lab.
-
-        Store an element (a labyrinth row) of :list:`master_list`,
-        which key is :int:`y`, as a list of elements.
-        Then access this list at key :int:`x` (column number in lab)
-        to replace its element by desired `object_to_place`.
-
-        Args:
-            master_list (list):
-                list representing the lab
-            x (int):
-                abs postion of object
-            y (int):
-                ord position of object
-            object_to_place (str):
-                element to place in lab.
-
-        Returns:
-            master_list (list):
-                list representing the lab with the new element in it.
-        """
-        master_list[y][x] = object_to_place
-        return master_list
-
-    def erase_from_lab(master_list: list, x: int, y: int):
-        """
-        Erase an element from the lab.
-
-        Args:
-            master_list (list):
-                list representing the lab
-            x (int):
-                abs postion of object
-            y (int):
-                ord position of object
-
-        Returns:
-            master_list (list):
-                list representing the lab with desired element removed
-        """
-        line = list(master_list[y])
-        line[x-1] = ' '
-        master_list[y] = line
-        return master_list
-
-class Positions():
-    pos_x: int 
-    pos_y: int
-
-    def reference_position():
-        
-        reference_position method defines point of origin and assign position
-        to each font item (empty path).
-        
+    def display_lab_in_console():
         pass
 
-    def up(self):
-        return self.pos_y + 1
 
-    def down(self):
-        return self.pos_y - 1
+class Characters(Maze):
 
-    def left(self):
-        return self.pos_x - 1
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+        self.state = True  # alive or dead/sleeping
+        if self.name == 'macgyver':
+            self.position = Maze.start.position
+        elif self.name == 'gard':
+            self.position = Maze.finish.position
 
-    def right(self):
-        return self.pos_x + 1
+    def disabled(self):
+        self.state = False
+
+    def mac_gic_touch():
+        pass
 
 
-class Characters(Positions):
-    pass
+class SomeItem(Maze):
+    """
+    choice_items_name = ('needle', 'tube', 'ether', 'syringe')
+    choice_item_nature = ('collectable', 'fabricable')
+    choice_items_property = ('mac', 'lab', None)
+    choice_items_state = ('to_find', 'found', 'used', 'made', 'to_fabricate')
+    """
 
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        self.position = position
+        if self.name == 'syringe':
+            self.nature = 'fabricable'
+            self.property = None
+            self.state = 'to_fabricate'
+        else:
+            self.nature = 'collectable'
+            self.property = 'lab'
+            self.state = 'to_find'
 
-class SomeItems(Maze, Positions):
+    def item_is_picked_up(self):
+        self.property = 'mac'
+        self.state = 'found'
+        pass
 
-    items_type = ['needle', 'tube', 'ether', 'syringe']
-    items_proprety = ['mac', 'gard', 'lab', None]
-    items_state = ['to_find', 'found', 'used', 'made']
-    pass
-"""
+    def assemble_items():
+        pass
