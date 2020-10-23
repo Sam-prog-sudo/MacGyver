dce75bfa8a8c55f4841310b4634841d099e9a79e
Sam <samysaad01@gmail.com>
2020-10-23 15:32:14 +0200

    Added a file with all constants.

---
+++ a_maze_ing/constants.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+"""
+Constants.py
+"""
+
+FILENAME = 'a_maze_ing.txt'
+
+DIMENSION = {
+    'width': 15,
+    'height': 15,
+    }
+
+DISPLAY = {
+    'path': ' '
+    'wall': 'X'
+    'gard': 'G'
+    'macgyver': 'M'
+    'start': 'S'
+    'finish': 'F'
+    'needle': 'n'
+    'tube': 't'
+    'ether': 'e'
+}
+
+CHOICE_ITEMS = {
+    'name': (
+        'needle',
+        'tube',
+        'ether',
+        'syringe',
+        ),
+    'nature': (
+        'collectable',
+        'fabricable',
+        ),
+    'property': (
+        'mac',
+        'lab',
+        None,
+        ),
+    'state': (
+        'to_find',
+        'found',
+        'used',
+        'made',
+        'to_fabricate',
+        ),
+    }
