55740603d1abd7db0c62c22f97ee5c50fdbba1d9
Sam <samysaad01@gmail.com>
2020-11-16 20:05:24 +0100

    Added property to item class.

---
+++ labyrinth/items.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+"""
+items.py
+"""
+from assets import constants as C
+
+
+class SomeItem:
+
+    def __init__(self, name: str, position_X, position_Y):
+        self.name = name
+        self.position_X = position_X
+        self.position_Y = position_Y
+        self.display = C.DISPLAY[self.name]
+        self.state = 'to_find'
+
+    @property
+    def position_tuple(self):
+        return (self.position_Y, self.position_X)
