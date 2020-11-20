da58807d2c14bf922e073c98d85f2253b02b6de5
Sam <samysaad01@gmail.com>
2020-11-20 19:39:59 +0100

    Modified Main class:
    - wrote docstring
    - modified still_awake
    - added check_char_pos
    - added item_picking_process
    - simplified/upgraded play method

--- operations/play.py
+++ operations/play.py
@@ -5,7 +5,6 @@ play.py
 # import pygame
 from assets.display import intro
 from labyrinth.maze import Maze
-# from assets import constants as C
 
 
 class Main:
@@ -15,24 +14,54 @@ class Main:
         intro()
         self.lab.print_lab()
 
-    def _still_alive(self):
-        if self.lab.macgyver.alive is False:
-            return "You're dead :("
-        elif self.lab.gard.alive is False:
-            return "You've beaten this bloody gard :)"
+    def still_awake(self):
+        """
+        still_awake verify if a character is still awake.
+
+        Returns:
+        - bool: if one of the characters is asleep.
+        """
+        if self.lab.macgyver.sleeping is True:
+            print("You''ve been caught :(")
+            return False
+        elif self.lab.gard.sleeping is True:
+            print("You've beaten this bloody gard :)")
+            return False
         else:
             return True
 
+    def _check_chars_pos(self):
+        """
+        Returns:
+            bool: if Macgyver and the gard are in the same room.
+        """
+        return self.lab.macgyver.position_tuple == self.lab.gard.position_tuple
+
+    def _item_picking_process(self):
+        """
+        _item_picking_process.
+
+        Iterates through the list of items.
+        if Macgyver and an item have the same position:
+        - Macgyver pick the item up
+        - the list of items is updated
+        - the list of empty paths to be displayed is updated.
+        """
+        for an_item in self.lab.list_items:
+            if self.lab.macgyver.position_tuple == an_item.position_tuple:
+                self.lab.macgyver.pick_up_item(an_item)
+                self.lab.update_list_items(an_item)
+                self.lab.update_list_empty_path(an_item)
+
     def play(self):
-        while self._still_alive():
+        """
+        Work hard, play hard !
+        """
+        while self.still_awake():
             direction = input('Enter valid move: ')
             self.lab.macgyver.move(direction, self.lab.list_paths)
-            self.lab.fight()
-            print(
-                "postion (x , y): ({} , {})".format(
-                    self.lab.macgyver.position_X,
-                    self.lab.macgyver.position_Y
-                    )
-                )
-            self.lab.macgyver.pick_up_item(self.lab.list_items)
+            if self._check_chars_pos():
+                self.lab.chars_meet_up()
+            if self.lab.list_items:
+                self._item_picking_process()
             self.lab.print_lab()
