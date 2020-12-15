98b9be4645ee80f1222ff9416007fbfcbcab1aea
Samy Saad <samy@emencia.com>
2020-12-15 23:51:44 +0100

    [NEW] Added pygame related constants.

--- assets/constants.py
+++ assets/constants.py
@@ -3,8 +3,9 @@
 constants.py
 """
 
+IMAGE_FOLDER = "assets/ressource/images/"
 LAB_TEMPLATE = "assets/templates/a_maze_ing.txt"
-INTRO_TEMPLATE = "assets/templates/intro_ascii_art.txt"
+INTRO_ART = "assets/ressource/arts/intro_ascii_art.txt"
 
 SIZE = {
     'width': 15,
@@ -39,3 +40,21 @@ CHOICE_ITEMS = {
     }
 
 VALID_DIRECTIONS = {'up', 'down', 'left', 'right'}
+
+IMAGES = {
+    'macgyver': 'MacGyver.png',
+    'gard': 'Gardien.png',
+    'needle': 'aiguille.png',
+    'tube': 'tube_plastique.png',
+    'ether': 'ether.png',
+    'seringe': 'seringue.png',
+    'wall': 'wall.png',
+    'path': 'floor.png'
+}
+
+WIDTH = int(600)  # each tile is 30 pixels large
+HEIGHT = int(600)
+FPS = int(10)
+A_MOVE = int(40)
+
+BLACK = (0, 0, 0)
