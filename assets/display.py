39f5e7bf73d26e9a1e1ae7a994cbd28edf44d81a
Samy Saad <samy@emencia.com>
2020-12-15 23:39:00 +0100

    [NEW] Added text for console output.

--- assets/display.py
+++ assets/display.py
@@ -4,6 +4,25 @@ display.py
 """
 from . import constants as C
 
+intro_text = """
+Welcome to a_maze_ing !
+
+Save Macgyver by exiting this labyrinth.
+Words of caution: beware of the gard !
+(he has COVID)
+
+Please select a platform:
+'g' for graphical interface
+'c' for console
+"""
+goodbye = "See you soon :)"
+inval_in = "Invalid input: "
+victory = "You've beaten this bloody gard :)"
+defeat = "You've been caught :("
+ask_move = 'Enter valid move: '
+item = "You've picked an item: "
+yahoo = "You've assembled all the items ! Go for IT !"
+
 
 def intro():
     """
