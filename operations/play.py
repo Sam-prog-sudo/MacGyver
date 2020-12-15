d56c3393ead0680ff8360ef5f5263e5ba086b0c1
Samy Saad <samy@emencia.com>
2020-12-15 23:49:51 +0100

    [NEW] Added console and gui option.

--- operations/play.py
+++ operations/play.py
@@ -2,9 +2,11 @@
 """
 play.py
 """
-# import pygame
-from assets.display import intro
+from assets import display as d
 from labyrinth.maze import Maze
+
+from operations.pg.game import Game
+from .interact import Interaction
 
 
 class Main:
@@ -12,54 +14,37 @@ class Main:
         self.lab = Maze()
         self.lab.create_lab_elements()
 
-    def still_awake(self):
-        """
-        still_awake verify if a character is still awake.
-
-        Returns:
-        - bool: if one of the characters is asleep.
-        """
-        if self.lab.macgyver.sleeping is True:
-            print("You''ve been caught :(")
-            return False
-        elif self.lab.gard.sleeping is True:
-            print("You've beaten this bloody gard :)")
-            return False
+    def select_plateform(self):
+        print(d.intro_text)
+        x = input()
+        if x == "g" or x == "G":
+            self.gui()
+        elif x == "c" or x == "C":
+            self.console()
         else:
-            return True
+            print(d.inval_in, x)
+            return self.select_plateform()
 
-    def _check_chars_pos(self):
-        """
-        Returns:
-            bool: if Macgyver and the gard are in the same room.
-        """
-        return self.lab.macgyver.position_tuple == self.lab.gard.position_tuple
+    def console(self):
+        d.intro()
+        d.print_lab(self.lab)
+        inter = Interaction(self.lab)
+        while inter.still_awake():
+            direction = input('\n'+d.ask_move)
+            self.lab.macgyver.move_char(direction, self.lab.list_paths)
+            if inter.check_chars_pos():
+                self.lab.chars_meet_up()
+            if self.lab.list_items:
+                inter.item_picking_process()
+            d.print_lab(self.lab)
 
-    def _item_picking_process(self):
-        """
-        _item_picking_process.
-
-        Iterates through the list of items.
-        if Macgyver and an item have the same position:
-        - Macgyver pick the item up
-        - the list of items is updated
-        - the list of empty paths to be displayed is updated.
-        """
-        for an_item in self.lab.list_items:
-            if self.lab.macgyver.position_tuple == an_item.position_tuple:
-                self.lab.macgyver.pick_up_item(an_item)
-                self.lab.update_list_items(an_item)
-                self.lab.update_list_empty_path(an_item)
+    def gui(self):
+        G = Game(maze=self.lab)
+        G.run()
+        print(d.goodbye)
 
     def play(self):
         """
         Work hard, play hard !
         """
-        while self.still_awake():
-            direction = input('Enter valid move: ')
-            self.lab.macgyver.move(direction, self.lab.list_paths)
-            if self._check_chars_pos():
-                self.lab.chars_meet_up()
-            if self.lab.list_items:
-                self._item_picking_process()
-            self.lab.print_lab()
+        self.select_plateform()
